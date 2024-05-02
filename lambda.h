2df4476f131eecf5546e476326abdd4c8b6cdd79
susuko <flokinyan@gmail.com>
2024-05-03 05:26:10 +0900

    Fix variadic macro to handle arguments with commas

--- lambda.h
+++ lambda.h
@@ -1,9 +1,9 @@
 #ifndef LAMBDA_H
 # define LAMBDA_H
 
-# define PRED(body) ({ bool _pred(void *_x) body _pred; })
-# define MAPPER(body) ({ void *_mapper(void *_x) body _mapper; })
-# define REDUCER(body) ({ void *_reducer(void *_s, void *_e) body _reducer; })
-# define ACTION(body) ({ void _action(void *_x) body _action; })
+# define PRED(...) ({ bool _pred(void *_x) __VA_ARGS__ _pred; })
+# define MAPPER(...) ({ void *_mapper(void *_x) __VA_ARGS__ _mapper; })
+# define REDUCER(...) ({ void *_reducer(void *_s, void *_e) __VA_ARGS__ _reducer; })
+# define ACTION(...) ({ void _action(void *_x) __VA_ARGS__ _action; })
 
 #endif
